a5840d5dd2547991f354571b0f5766c71771fd19
TRUPTIPATEL2512 <156118870+TRUPTIPATEL2512@users.noreply.github.com>
2025-01-25 14:30:27 -0500

    Initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.hellowearapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.hellowearapp"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.play.services.wearable)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+}
